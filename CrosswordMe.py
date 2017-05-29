72976cfe08b05d97050432aa8334361fda64b05c
Sebastian Latacz <sebastian@gitmate.io>
2017-05-29 16:21:04 +0200

    Works for up to 9 words. Code cleaned up.

--- CrosswordMe.py
+++ CrosswordMe.py
@@ -2,7 +2,7 @@ from random import randint
 
 #input
 
-words = ["one", "two", "three", "four", "five", "six"]
+words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 user_input = ""
 
 """while 1 == 1:
@@ -13,25 +13,30 @@ user_input = ""
 
 # creating the puzzle
 ## horizontals
-line = randint(0,10)
-lines_used = []
-puzzle = [["$" for x in range(10)] for y in range(10)]
-for word in range(0, len(words)):
-    lines_used.append(line)
-    word_position = 0
-    #print(len(words[word]))
-    start_position = randint(0, 10-len(words[word]))
-    end_position = start_position+len(words[word])
-    for array_position in range(start_position, end_position):
-        puzzle[line][array_position] = words[word][word_position]
-        word_position += 1
-    line = randint(0,10)
-    while line in lines_used:
-        line = randint(0,10)
-print(words)
+line = randint(0,9) #First line to write in
+lines_used = [] #Empty array which contains lines which have been written in already (can probably be removed when its possible to write multiple words in one line)
 
-#puzzle[2][1] = 1
-#print(puzzle[2]) prints the entire second line
+puzzle = [["$" for x in range(10)] for y in range(10)] #Creates 10x10 array filled with "$"
+
+for word in range(0, len(words)): #loops through every word in the words list
+
+    lines_used.append(line) #appends used lines to used lines list
+
+    word_position = 0 #resets the word_position. this is needed beacuse word_position counts through the length of every word being written
+
+    start_position = randint(0, 10-len(words[word])) #int for start_position
+    end_position = start_position+len(words[word]) #int for end_position
+
+    for array_position in range(start_position, end_position): #Loops through position within the array
+
+        puzzle[line][array_position] = words[word][word_position] #Writes the word at random position of the array
+        word_position += 1 #Fake loop for the word position
+
+    line = randint(0,9) #Choosees new line
+    while line in lines_used: #keeps creating a new line as long as the line has been used alreard
+        line = randint(0,9)
+
+
 #Prints the puzzle
 for line in range(10):
     for position in range(10):

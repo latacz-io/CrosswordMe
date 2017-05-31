987be2af3ba98e2812dd96747fea27580306064f
seblat <sebastian.lbb@gmail.com>
2017-05-31 21:06:32 +0200

    Bisher drin: Wenn das Wort komplett gleich ist. Es fehlt: Wenn das wort zum Teil gleich ist. Mit in lösen? Oder lieber ganze Zeile vergleichen?

--- CrosswordMe.py
+++ CrosswordMe.py
@@ -10,7 +10,8 @@ def print_puzzle(puzzle, line_count, column_count):
 
 def word_input():
     #Collects the words for the puzzle
-    words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+    words = ["one"] * 100
+    #, "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
     user_input = ""
     """while 1 == 1:
         user_input = input("Word: ")
@@ -30,19 +31,27 @@ def create_puzzle(words, line_count, column_count):
     line_order = sample(range(line_count), line_count)
     puzzle = [["$" for x in range(column_count)] for y in range(line_count)] #Creates 10x10 array filled with "$"
 
-    for word, line in zip(range(len(words)), line_order): #loops through every word in the words list
-
-
-
+    for word in range(len(words)): #loops through every word in the words list
         word_position = 0 #resets the word_position. this is needed beacuse word_position counts through the length of every word being written
+        line = line_order[0] #TEMP
+        if word == 0:
+            start_position = randint(0, 10-len(words[word])) #int for start_position
+            end_position = start_position+len(words[word]) #int for end_position
 
-        start_position = randint(0, 10-len(words[word])) #int for start_position
-        end_position = start_position+len(words[word]) #int for end_position
+            for array_position in range(start_position, end_position): #Loops through position within the array
 
-        for array_position in range(start_position, end_position): #Loops through position within the array
+                puzzle[line][array_position] = words[word][word_position] #Writes the word at random position of the array
+                word_position += 1 #Fake loop for the word position
 
-            puzzle[line][array_position] = words[word][word_position] #Writes the word at random position of the array
-            word_position += 1 #Fake loop for the word position
+        else:
+            for array_position in range(column_count-len(words[word])+1):
+                if puzzle[line][array_position:array_position+len(words[word])-1] == words[word]: #Bisher drin: Wenn das Wort komplett gleich ist. Es fehlt: Wenn das wort zum Teil gleich ist. Mit "in" lösen?
+                     puzzle[line][array_position:array_position+len(words[word])-1] = words[word]
+
+
+
+
+
     return puzzle
 
 words = word_input()

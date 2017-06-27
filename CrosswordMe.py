26ca73996b44d04c78575b98477f98fc2c1cf6ae
latacz-io <sebastian.lbb@gmail.com>
2017-06-27 19:30:10 +0200

    Fit write_vertical to new architecture

--- CrosswordMe.py
+++ CrosswordMe.py
@@ -58,23 +58,19 @@ def calculate_fits(word, word_index, puzzle):
 def write_horizontal(words, word_index, puzzle, start_x_position, start_y_position):
 
     word_position = 0 #resets the word_position. this is needed beacuse word_position counts through the length of every word being written
-    for puzzle_x_position in range(start_x_position, start_x_position+len(words[word_index])): #Loops through position within the array
+    for puzzle_x_position in range(start_x_position, start_x_position+len(words[word_index])): #Loops through position within the puzzle
 
         puzzle[start_y_position][puzzle_x_position] = words[word_index][word_position] #Writes the word at random position of the array
         word_position += 1 #Fake loop for the word position
 
     return puzzle
 
-def write_vertical(words, word_index, puzzle):
-    if word_index == 0:
+def write_vertical(words, word_index, puzzle, start_x_position, start_y_position):
 
-        start_x_position, start_y_position, end_x_position, end_y_position = calculate_postions(len(words[word_index]))
-        column = randint(0, column_count-1)
+    word_position = 0 #resets the word_position. this is needed beacuse word_position counts through the length of every word being written
+    for puzzle_y_position in range(start_y_position, start_y_position+len(words[word_index])): #Loops through position within the array
 
-        word_position = 0 #resets the word_position. this is needed beacuse word_position counts through the length of every word being written
-        for array_position in range(start_y_position, end_y_position): #Loops through position within the array
-
-            puzzle[array_position][column] = words[word_index][word_position] #Writes the word at random position of the array
+            puzzle[puzzle_y_position][start_x_position] = words[word_index][word_position] #Writes the word at random position of the array
             word_position += 1 #Fake loop for the word position
 
     return puzzle
@@ -106,7 +102,7 @@ def create_puzzle(words):
             if word_direction == 0:
                 puzzle = write_horizontal(words, word_index, puzzle, start_x_position, start_y_position)
             elif word_direction == 1:
-                puzzle = write_vertical(words, word_index, puzzle)
+                puzzle = write_vertical(words, word_index, puzzle, start_x_position, start_y_position)
             elif word_direction == 2:
                 puzzle = write_diagonal(words, word_index, puzzle)
 

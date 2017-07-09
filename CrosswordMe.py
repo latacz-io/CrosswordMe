0e967d3e99b02f58e8347216b6ddaf7b6c27bf83
Sebastian Latacz <sebastian@gitmate.io>
2017-07-09 07:50:34 +0200

    Changed names of Line and Columt count to caps

--- CrosswordMe.py
+++ CrosswordMe.py
@@ -4,18 +4,18 @@ from string import ascii_letters
 
 def print_puzzle(puzzle):
     #Prints the puzzle puzzle on a board
-    for line in range(line_count):
-        for position in range(column_count):
+    for line in range(LINE_COUNT):
+        for position in range(COLUMN_COUNT):
             print(str(puzzle[line][position]).upper() + " ", end="") #Prints character in current position
         print() #Prints Newline
 
 def fill_field_with_randoms(puzzle):
-    #Fills the rest of the free space (represented by "$") with random letters
+    #Fills the rest of the free space (represented by " ") with random letters
 
-    for puzzle_y_position in range(line_count):
-        for puzzle_x_position in range(column_count):
+    for puzzle_y_position in range(LINE_COUNT):
+        for puzzle_x_position in range(COLUMN_COUNT):
 
-            if puzzle[puzzle_y_position][puzzle_x_position] == " ":
+            if puzzle[puzzle_y_position][puzzle_x_position] == " ": #If the position is empty
                 puzzle[puzzle_y_position][puzzle_x_position] = choice(ascii_letters)
 
 
@@ -27,8 +27,8 @@ def word_input():
 
     words = ["Test", "Noch", "Nocheiner", "loooooooooooooooooooooong"]
     for word in words:
-        if len(word) > line_count:
-            print(word + " has " + str(len(word)) + " letters. A maximum length of " + str(line_count) + " Letters is possible. " + word + " has been ommitted" )
+        if len(word) > LINE_COUNT:
+            print(word + " has " + str(len(word)) + " letters. A maximum length of " + str(LINE_COUNT) + " Letters is possible. " + word + " has been ommitted" )
             words.remove(word)
     words.sort(key=len, reverse=True)
 
@@ -36,10 +36,10 @@ def word_input():
 
 def define_field_size():
     #defines field size
-    global line_count
-    line_count = 12
-    global column_count
-    column_count = 12
+    global LINE_COUNT
+    LINE_COUNT = 12
+    global COLUMN_COUNT
+    COLUMN_COUNT = 12
 
 def set_direction_parameters(direction):
     #returns direction parameters
@@ -61,15 +61,15 @@ def calculate_fits(puzzle, current_word):
 
     fit = 0
 
-    for puzzle_y_position in sample(range(line_count), k = line_count): #loops through every position in the lines in random order
-        for puzzle_x_position in sample(range(column_count), k = column_count): #loops through every position in the columns in random order
+    for puzzle_y_position in sample(range(LINE_COUNT), k = LINE_COUNT): #loops through every position in the lines in random order
+        for puzzle_x_position in sample(range(COLUMN_COUNT), k = COLUMN_COUNT): #loops through every position in the columns in random order
             for fit_direction in sample(range(3), k = 3):
 
                 x_activator, y_activator = set_direction_parameters(fit_direction)
 
 
                 temp_fit = 0
-                if (puzzle_x_position + len(current_word)) * x_activator <= column_count and (puzzle_y_position + len(current_word)) * y_activator <= line_count: #Makes shure the temp Puzzle index doenst get out of range  to the right
+                if (puzzle_x_position + len(current_word)) * x_activator <= COLUMN_COUNT and (puzzle_y_position + len(current_word)) * y_activator <= LINE_COUNT: #Makes shure the temp Puzzle index doenst get out of range  to the right
 
                     for word_position in range(len(current_word)):
 
@@ -100,12 +100,12 @@ def calculate_fits(puzzle, current_word):
 
         start_x_position = 0 #this is used for breaking out of the for loops
 
-        for puzzle_y_position in sample(range(line_count), k = line_count): #loops through every position in the lines in random order
-            for puzzle_x_position in sample(range(column_count), k = column_count): #loops through every position in the columns in random order
+        for puzzle_y_position in sample(range(LINE_COUNT), k = LINE_COUNT): #loops through every position in the lines in random order
+            for puzzle_x_position in sample(range(COLUMN_COUNT), k = COLUMN_COUNT): #loops through every position in the columns in random order
                 for fit_direction in sample(range(3), k = 3): #loops through every direction in random order
 
                     x_activator, y_activator = set_direction_parameters(fit_direction)
-                    if (puzzle_x_position + len(current_word)) * x_activator <= column_count and (puzzle_y_position + len(current_word)) * y_activator <= line_count: #Makes shure the temp Puzzle index doenst get out of range  to the right
+                    if (puzzle_x_position + len(current_word)) * x_activator <= COLUMN_COUNT and (puzzle_y_position + len(current_word)) * y_activator <= LINE_COUNT: #Makes shure the temp Puzzle index doenst get out of range  to the right
                         for word_position in range(len(current_word)):
                             if puzzle[puzzle_y_position + (word_position * y_activator)][puzzle_x_position + (word_position * x_activator)] != current_word[word_position] and puzzle[puzzle_y_position + (word_position * y_activator)][puzzle_x_position + (word_position * x_activator)] != " ": #if the ltter doesn match and the puzzle postion isnt empty (" ") this breaks the loop
                                 #the word doesnt fit in this position, breaks out of word loop
@@ -143,14 +143,14 @@ def write_puzzle(puzzle, current_word, start_x_position, start_y_position, word_
 
 def create_puzzle(words):
     # creates an array with " " symbols to be the field and loops through the words being written
-    puzzle = [[" " for x in range(column_count)] for y in range(line_count)] #Creates array filled with " "
+    puzzle = [[" " for x in range(COLUMN_COUNT)] for y in range(LINE_COUNT)] #Creates array filled with " "
 
     for word_index in range(len(words)): #loops through every word in the words list
 
         if word_index == 0: #Special case for the first word which is written in a random position in a random direction on the board
             word_direction = randint(0,2) #Random direction
-            start_x_position = randint(0, column_count-len(words[word_index])) #Random x position within the boundries
-            start_y_position = randint(0, line_count-len(words[word_index])) #Random y position within the boundries
+            start_x_position = randint(0, COLUMN_COUNT-len(words[word_index])) #Random x position within the boundries
+            start_y_position = randint(0, LINE_COUNT-len(words[word_index])) #Random y position within the boundries
             puzzle = write_puzzle(puzzle, words[word_index], start_x_position, start_y_position, word_direction)
 
 
